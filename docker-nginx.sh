5a3a6c0384a44d291b938a53f140648e8aa5ed6d
James Grieve <jamesonrgrieve@gmail.com>
2024-10-16 13:14:05 -0600

    Fix SSL;

--- docker-nginx.sh
+++
@@ -1,5 +0,0 @@
-docker-compose -f docker-compose-nginx.yml down
-docker-compose -f docker-compose-nginx.yml pull
-docker-compose -f docker-compose-nginx.yml build
-docker-compose -f docker-compose-nginx.yml up -d
-docker-compose -f docker-compose-nginx.yml logs --follow
--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
-docker-compose down
-docker-compose pull
-docker-compose build
-docker-compose up -d
-docker-compose logs --follow
+docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml logs --follow
